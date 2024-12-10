147c8bfdbf6de71bf7bf7b0d8a81debf9971bd8d
LAPTOP-SVESTDUI\sylvia <sylviacooperhouse@gmail.com>
2024-12-10 14:18:17 -0800

    small modifications

--- investment_db/assets.sql
+++ investment_db/assets.sql
@@ -1,5 +1,25 @@
 CREATE TABLE assets (
     asset_id SERIAL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    type VARCHAR(50) NOT NULL
+    asset_name VARCHAR(255) NOT NULL,
+    asset_type VARCHAR(50) NOT NULL
 );
+
+
+--DROP TABLE assets
+
+INSERT INTO assets (asset_name, asset_type) VALUES
+('Bitcoin', 'crypto'),
+('Ethereum', 'crypto'),
+('Dogecoin', 'crypto'),
+('AAPL', 'stock'),
+('GOOG', 'stock'),
+('INTC', 'stock'),
+('K', 'stock'),
+('MSFT', 'stock'),
+('PFE', 'stock');
+
+ALTER TABLE assets
+RENAME COLUMN name TO asset_name;
+
+ALTER TABLE assets
+RENAME COLUMN type TO asset_type;
--- investment_db/market_data.sql
+++ investment_db/market_data.sql
@@ -1,16 +1,11 @@
+--DROP TABLE market_data
 CREATE TABLE market_data (
-    market_id SERIAL PRIMARY KEY,
     asset_id INT REFERENCES assets(asset_id),
     date DATE NOT NULL,
     open NUMERIC NOT NULL,
     close NUMERIC NOT NULL,
     high NUMERIC,
     low NUMERIC,
-    volume BIGINT
+    volume BIGINT,
+    PRIMARY KEY (asset_id, date)
 );
-
-
-ALTER TABLE market_data
-ADD CONSTRAINT fk_market
-FOREIGN KEY (market_id)
-REFERENCES markets (market_id);
